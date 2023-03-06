9e891f6ca57344908ca1db87141cac8f1a2dd5f2
dawoonkim <hec8897@naver.com>
2023-03-06 12:47:18 +0900

    create Header
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/component/common/Header.jsx
+++ src/component/common/Header.jsx
@@ -1,5 +1,24 @@
 import * as React from "react";
+import { Screen } from "component";
+import { Link } from "gatsby";
 
-const Header = () => <div>헤더</div>;
+const Header = () => (
+  <Screen>
+    <div className="flex justify-between items-center py-4">
+      <h1 className="font-bold text-xl">DawoonRoad</h1>
+      <ul className="flex gap-4 items-center">
+        <li>
+          <Link to="about">About</Link>
+        </li>
+        <li>
+          <Link to="posting">posting</Link>
+        </li>
+        <li>
+          <Link to="develop">develop</Link>
+        </li>
+      </ul>
+    </div>
+  </Screen>
+);
 
 export default Header;
