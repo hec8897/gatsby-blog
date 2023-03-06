71e0b4df7ed9802cf256dd8577a848fd91025768
dawoonkim <hec8897@naver.com>
2023-03-06 11:41:50 +0900

    add tailwind
    Signed-off-by: dawoonkim <hec8897@naver.com>

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "../styles/global.css";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline text-red-500">123</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
