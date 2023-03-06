c0fbef1838635eb73f3e88b7e578e93291889e81
dawoonkim <hec8897@naver.com>
2023-03-06 13:39:00 +0900

    content list object
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/component/common/Card.jsx
+++ src/component/common/Card.jsx
@@ -1,23 +1,29 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
-const Card = () => (
-  <div className="h-[280px] rounded border drop-shadow-md bg-white p-2 flex flex-col justify-between gap-1">
-    <div className="h-[50%] rounded bg-green-200 flex items-center justify-center">
-      Dawoon
-    </div>
-    <div>
-      <h2 className="text-lg font-bold">Lorem ipsum</h2>
+const Card = ({ data }) => {
+  const { title, date, tagList, id, link, summary } = data;
+  return (
+    <div className="h-[280px] rounded border drop-shadow-md bg-white p-2 flex flex-col justify-between">
+      <div className="h-[50%] rounded bg-green-200 flex items-center justify-center">
+        {title}
+      </div>
+      <div>
+        <h2 className="text-lg font-bold hover:underline cursor-pointer">
+          <Link to={link + "/" + id}>{title}</Link>
+        </h2>
+        <p className="text-xs">{summary}</p>
+        <p className="text-right">
+          <span className="text-xs text-gray-500">{date}</span>
+        </p>
+      </div>
       <p className="text-xs text-blue-400 flex gap-1 flex-wrap">
-        <span>#React</span>
-        <span>#Javascript</span>
-        <span>#Typescript</span>
-        <span>#FrontEnd</span>
-        <span>#Pattern</span>
+        {tagList.map((tag, index) => (
+          <span key={index}>#{tag}</span>
+        ))}
       </p>
-      <span className="text-xs text-gray-500">2023-01-01</span>
     </div>
-    <div className="bg-blue-300 py-2 text-center rounded-xl">Action</div>
-  </div>
-);
+  );
+};
 
 export default Card;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,34 @@ import * as React from "react";
 import "styles/global.css";
 import { Header, Screen, Card } from "component";
 
+const CONTENT_LIST = [
+  {
+    id: 0,
+    title: "title",
+    link: "FE",
+    summary: "Lorem ipsum dolor sit consectetur adipisicing.",
+    tagList: ["JavaScript", "TypeScript"],
+    date: "2023-01-01",
+  },
+  {
+    id: 1,
+    title: "title",
+    link: "FE",
+    summary: "Lorem ipsum dolor sit consectetur adipisicing.",
+    tagList: ["JavaScript", "TypeScript"],
+    date: "2023-01-01",
+  },
+];
+
 const IndexPage = () => {
   return (
     <main>
       <Header />
       <Screen>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 my-4 gap-4">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {CONTENT_LIST.map((ele) => (
+            <Card key={ele.id} data={ele} />
+          ))}
         </div>
       </Screen>
     </main>
