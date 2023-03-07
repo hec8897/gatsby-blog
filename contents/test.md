fa13b5e152b9b2253229dcdd0fec256c62b52a93
김다운 <37240562+hec8897@users.noreply.github.com>
2023-03-07 14:08:35 +0900

    MarkDown 기본 설정 (#5)
    * require plugin install
    
    Signed-off-by: dawoonkim <hec8897@naver.com>
    
    * 템플릿 생성 기본 설정
    
    Signed-off-by: dawoonkim <hec8897@naver.com>
    
    * markdown style
    
    Signed-off-by: dawoonkim <hec8897@naver.com>
    
    ---------
    
    Signed-off-by: dawoonkim <hec8897@naver.com>

---
+++ contents/test.md
@@ -0,0 +1,61 @@
+---
+date: "2020-07-29"
+title: "Test2"
+categories: ["Web", "SEO", "Optimization"]
+summary: "홈페이지를 운영하는 많은 사람들 또는 기업들이 검색 페이지 최상단에 보여지게 하기 위해 어떤 최적화 작업을 하는지 알아보자."
+---
+
+# Head1
+
+---
+
+## Head2
+
+### Head3
+
+#### Head4
+
+text
+
+## _italic_
+
+**bold**
+
+**_italic and bold_**
+
+## List
+
+- Gatsby
+  - docs
+  - docs
+  - docs
+
+1. Gatsby
+2. docs
+3. docs
+
+| foo | bar |
+| :-- | :-: |
+| baz | qux |
+
+~~strikethrough~~
+
+![alt text](../src//images//cat.jpg)
+
+[naver](https://www.naver.com)
+
+> blockquote
+>
+> > nested blockquote
+>
+> > **I'm bold!**
+>
+> more quotes
+
+`console.log(1)`
+
+```jsx
+const test = 1;
+```
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4n0xNbfJLR8" frameborder="0" allowfullscreen></iframe>
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,66 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby + Vercel`,
+    title: `Dawoon-blog`,
     siteUrl: `https://gatsby-template.vercel.app/`,
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-resolve-src"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-resolve-src",
+    "gatsby-plugin-image",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `./src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `contents`,
+        path: `./contents`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-smartypants",
+            options: {
+              dashes: "oldschool",
+            },
+          },
+          {
+            resolve: "gatsby-remark-prismjs",
+            options: {
+              classPrefix: "language-",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 768,
+              quality: 100,
+              withWebp: true,
+            },
+          },
+          {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {},
+          },
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "nofollow",
+            },
+          },
+        ],
+      },
+    },
+  ],
 };
