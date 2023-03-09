308b428129c77eb2e0b537e45ce5248ace270b06
dawoonkim <hec8897@naver.com>
2023-03-09 12:45:39 +0900

    title 수정
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/component/common/Title.jsx
+++ src/component/common/Title.jsx
@@ -3,8 +3,8 @@ import { CategoryList, GatsbyImageComponent } from "component";
 
 const Title = ({ children, date, categories, image }) => (
   <div className="flex flex-col gap-1">
-    <h1 className="font-bold">{children}</h1>
-    <p>{date}</p>
+    <h1 className="font-bold text-2xl">{children}</h1>
+    <p className="text-sm text-gray-500">{date}</p>
     <CategoryList categories={categories} />
     <GatsbyImageComponent gatsbyImageData={image} alt="썸네일" />
   </div>
