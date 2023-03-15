d8175d7cc826ef95d35b827bf684ebc6a445d5fe
hec8897 <hec8897@naver.com>
2023-03-15 22:19:19 +0900

    series ui 추가
    Signed-off-by: hec8897 <hec8897@naver.com>

--- src/component/index.js
+++ src/component/index.js
@@ -4,6 +4,7 @@ export { default as CategoryList } from "./common/CategoryList";
 export { default as GatsbyImageComponent } from "./common/GatsbyImageComponent";
 export { default as Title } from "./common/Title";
 export { default as CommonHead } from "./common/Head";
+export { default as SeriseList } from "./common/SeriseList";
 
 export { default as Screen } from "./ui/Screen";
 export { default as Template } from "./template";
